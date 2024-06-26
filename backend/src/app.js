86485a4c03b1d1d724600b5e298f3b8243cae3ed
Huzaifa475 <hskhan4759@gmail.com>
2024-06-26 07:58:48 +0530

    fixing

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ import mongoose from 'mongoose';
 const app = express();
 
 app.use(cors({
-    origin: ["https://expense-tracker-kwm6-frontend.vercel.app"],
+    origin: ["https://expense-tracker-two-weld.vercel.app/"],
     methods: ["POST", "GET", "DELETE", "PATCH"],
     credentials: true
 }));
